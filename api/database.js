ed123da82e201fb461944c3977fc691185093ea9
Martini <166950628+martinosalasm@users.noreply.github.com>
2025-04-29 13:46:03 -0400

    fixed database.js fil, added controllers

--- api/database.js
+++ api/database.js
@@ -1,11 +1,17 @@
 import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
-const pool = mysql
-  .createPool({
-    host: "process.env.DB_HOST",
-    user: "process.env.DB_USER",
-    password: "process.env.DB_PASSWORD",
-    database: "process.env.DB_NAME",
-  })
-  .promise();
+const db = mysql
+  .createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+
+  db.connect(err => {
+    if (err) throw err;
+    console.log('✅ MySQL connected');
+  });
+
+  export default db;
