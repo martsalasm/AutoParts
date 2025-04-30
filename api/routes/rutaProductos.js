4abb4a3f1957014c73c9ef6c8bd602ca1c9995db
Martini <166950628+martinosalasm@users.noreply.github.com>
2025-04-30 02:07:58 -0400

    syntax

--- api/routes/rutaProductos.js
+++ api/routes/rutaProductos.js
@@ -1,22 +1,11 @@
 import express from "express";
 const router = express.Router();
+import getProductos from "../controllers/productoController.js";
 
-//router.route("/").get((req, res) => {
-//    res.send("Lista de productos");
-//  })
-//.put((req, res) => {
-//    res.send("Producto actualizado");
-//  })
-// .post((req, res) => {
-//    res.send("Producto creado");
-//  });
 router.get("/:id", (req, res) => {
   res.send(`Producto con id: ${req.params.id}`);
 });
 
-// async function getProductsById() {
-// const products = await pool.query("SELECT * FROM products");
-// return products;}
 router.put("/:id", (req, res) => {
   res.send(`Producto con id: ${req.params.id} actualizado`);
 });
@@ -25,8 +14,4 @@ router.delete("/:id", (req, res) => {
   res.send(`Producto con id: ${req.params.id} eliminado`);
 });
 
-router.param("id", (req, res, next, id) => {
-  console.log(`ID del producto: ${id}`);
-  next();
-});
 export default router;
