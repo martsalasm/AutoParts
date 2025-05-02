92f1e48dfd31b5dbbe13bb2bf4dba04bc269257a
Martini <166950628+martinosalasm@users.noreply.github.com>
2025-05-02 02:06:08 -0400

    orden

--- api/routes/rutaProductos.js
+++ api/routes/rutaProductos.js
@@ -3,8 +3,8 @@ const router = express.Router();
 import productoController from "../controllers/productoController.js";
 
 router.get("/", productoController.getProductos);
-router.post("/", productoController.addProducto);
 router.get("/:id", productoController.getProductoById);
+router.post("/", productoController.addProducto);
 router.delete("/:id", productoController.deleteProductoById);
 router.put("/:id", productoController.updateProductoById);
 export default router;
