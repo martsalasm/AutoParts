5a8dc9b01bc5058a52d0d07f1565dcf849887b6b
Martini <mart.salasm@duocuc.cl>
2025-05-02 17:20:14 -0400

    added cors

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 app.use(express.json());
+app.use(cors());
 import empleadoRouter from "./routes/rutaEmpleados.js";
 import productRouter from "./routes/rutaProductos.js";
 
