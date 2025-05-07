bd802576a565307c3c62903415e2bd130260f92f
Martini <166950628+martinosalasm@users.noreply.github.com>
2025-05-06 21:48:49 -0400

    productos destacados, base

--- front-end/script.js
+++ front-end/script.js
@@ -21,6 +21,33 @@ document.addEventListener("DOMContentLoaded", ()=>{
     });
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+    const mostSoldProductsContainer = document.getElementById("most-sold");
+    fetch("http://localhost:3000/productos")
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(product => {
+            if (product.id_producto === 3 || product.id_producto == 6 || product.id_producto == 9) {
+                const productCard = document.createElement("div");
+                productCard.classList.add("product-card");
+                productCard.innerHTML = `
+                    <h2 class=card-title>${product.nombre_producto}</h2>
+                    <img src="${product.url_imagen}" alt="${product.nombre_producto}">
+                    <p>Precio: $${product.precio}</p>
+                    <p>Precio Mayorista: $${product.preciob2b}</p>
+                    <p> Stock: ${product.stock} unidades</p>
+                    <button class="add-to-cart" data-id="${product.id_producto}">Agregar al carrito</button>
+                `;
+                mostSoldProductsContainer.appendChild(productCard);
+            }
+        });
+    }).catch(error => {
+        console.error("Error al cargar los productos más vendidos:", error);
+    });
+
+
+
+});
 
 document.getElementById("categorias-title").addEventListener("click", () => {
     var categoriasList = document.getElementsByClassName("categorias-list");
