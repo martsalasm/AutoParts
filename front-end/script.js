e67ebccdef62b9765f1a66468ca0385dbab905ff
Martini <166950628+martinosalasm@users.noreply.github.com>
2025-05-15 22:43:37 -0400

    script separado para pcatalogo de productos

--- front-end/script.js
+++ front-end/script.js
@@ -1,26 +1,3 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    const productsContainer = document.getElementById("products-container");
-    fetch("http://localhost:3000/productos")
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(product => {
-            const productCard = document.createElement("div");
-            productCard.classList.add("product-card");
-            productCard.innerHTML = `
-                <h2 class=card-title>${product.nombre_producto}</h2>
-                <img src="${product.url_imagen}" alt="${product.nombre_producto}">
-                <p>Precio: $${product.precio}</p>
-                <p>Precio Mayorista: $${product.preciob2b}</p>
-                <p> Stock: ${product.stock} unidades</p>
-                <button class="add-to-cart" data-id="${product.id_producto}">Agregar al carrito</button>
-            `;
-            productsContainer.appendChild(productCard);
-        });
-    }).catch(error => {
-        console.error("Error al cagar los productos:", error);
-    });
-});
-
 document.addEventListener("DOMContentLoaded", () => {
     const mostSoldProductsContainer = document.getElementById("most-sold");
     fetch("http://localhost:3000/productos")
@@ -49,12 +26,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
-document.getElementById("categorias-title").addEventListener("click", () => {
-    var categoriasList = document.getElementsByClassName("categorias-list");
-
-    for (let i = 0; i < categoriasList.length; i++) {
-        if (window.innerWidth <= 600) {
-            categoriasList[i].classList.toggle('visible');
-        }
-    }
-});
