4fefa3ed1178e6c601caad892ae1e26026101000
Martini <166950628+martinosalasm@users.noreply.github.com>
2025-06-04 07:52:13 -0400

    destacados ahora dinamico

--- front-end/indexscript.js
+++ front-end/indexscript.js
@@ -3,27 +3,21 @@ import cart from "./cart.js";
 //función para mostrar los productos destacados
 document.addEventListener("DOMContentLoaded", () => {
   const mostSoldProductsContainer = document.getElementById("most-sold");
-  fetch("http://localhost:3000/productos")
+  fetch("http://localhost:3000/categorias/productos/1")
     .then((response) => response.json())
     .then((data) => {
       data.forEach((product) => {
-        if (
-          product.id_producto === 3 ||
-          product.id_producto == 6 ||
-          product.id_producto == 9
-        ) {
-          const productCard = document.createElement("div");
-          productCard.classList.add("product-card");
-          productCard.innerHTML = `
-                    <h2 class=card-title>${product.nombre_producto}</h2>
-                    <img src="${product.url_imagen}" alt="${product.nombre_producto}">
-                    <p>Precio: $${product.precio}</p>
-                    <p>Precio Mayorista: $${product.preciob2b}</p>
-                    <p> Stock: ${product.stock} unidades</p>
-                    <button class="add-to-cart" data-id="${product.id_producto}">Agregar al carrito</button>
-                `;
-          mostSoldProductsContainer.appendChild(productCard);
-        }
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+        productCard.innerHTML = `
+          <h2 class="card-title">${product.nombre_producto}</h2>
+          <img src="${product.url_imagen}" alt="${product.nombre_producto}">
+          <p>Precio: $${product.precio}</p>
+          <p>Marca: ${product.marca}</p>
+          <p>Stock: ${product.stock} unidades</p>
+          <button class="add-to-cart" data-id="${product.id_producto}">Agregar al carrito</button>
+        `;
+        mostSoldProductsContainer.appendChild(productCard);
       });
       document.querySelectorAll(".add-to-cart").forEach((button) => {
         button.addEventListener("click", (event) => {
